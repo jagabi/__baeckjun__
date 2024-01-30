1b8b64ae9627aee622424d70bb0c7bf461eea7ca
jagabi <yundong0913@gmail.com>
2024-01-30 20:52:13 +0900

    2024-01-30

---
+++ 1_java/Main_1001.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main_1001 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int A = input.nextInt();
+        int B = input.nextInt();
+
+        System.out.println(A-B);
+
+        input.close();
+    }
+}
--- 1_python/Main_1000.py
+++ 1_python/Main_1000.py
@@ -1,1 +1,2 @@
-print("Hello World!")
+A,B = input().split()
+print(int(A)+int(B))
