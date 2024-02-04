68a03c955c4535cff9eafa88545a0bee218d6724
jagabi <yundong0913@gmail.com>
2024-02-04 14:30:59 +0900

    2024-02-04

---
+++ 1_java/Main_18108.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main_18108 {
+    public static void main(String[] args) {
+        Scanner year = new Scanner(System.in);
+        
+        int A = year.nextInt();
+
+        System.out.println(A-543);
+    }
+}
